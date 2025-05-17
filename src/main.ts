37d6e88d9a0fc7e05c19779c492fca8ae02e8f49
jim <f0920515972@gmail.com>
2025-05-17 17:14:27 +0800

    feat:安裝jspdf

--- src/main.ts
+++ src/main.ts
@@ -1,19 +1,20 @@
-import './assets/main.css'
+import "./assets/main.css";
 
-import { createApp } from 'vue'
-import { createPinia } from 'pinia'
+import { createApp } from "vue";
+import { createPinia } from "pinia";
 
-import App from './App.vue'
-import router from './router'
-import VueRouterTransition from '@duannx/vue-router-transition'
-import 'animate.css';
+import App from "./App.vue";
+import router from "./router";
+import VueRouterTransition from "@duannx/vue-router-transition";
+import "animate.css";
+import jsPDF from "jspdf";
 
+const app = createApp(App);
+const pinia = createPinia();
 
-const app = createApp(App)
-const pinia = createPinia()
+app.use(pinia);
+app.use(jsPDF);
+app.use(router);
+app.use(VueRouterTransition);
 
-app.use(pinia)
-app.use(router)
-app.use(VueRouterTransition)
-
-app.mount('#app')
+app.mount("#app");
