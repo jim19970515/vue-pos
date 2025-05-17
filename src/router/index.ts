50fcc7b82cd71d2175a629e9779a249095b853c1
jim <f0920515972@gmail.com>
2025-05-17 17:15:15 +0800

    feat: 新增Details頁面

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,68 +1,74 @@
-import { createRouter, createWebHistory } from 'vue-router'
-import Pos from '../views/Pos.vue'
-import Order from '../views/Order.vue'
-import Unpaid from '@/views/Unpaid.vue'
-import Paid from '@/views/Paid.vue'
-import { useOrderStore } from '@/stores/modules/orders'
+import { createRouter, createWebHistory } from "vue-router";
+import Pos from "../views/Pos.vue";
+import Order from "../views/Order.vue";
+import Unpaid from "@/views/Unpaid.vue";
+import Paid from "@/views/Paid.vue";
+import Details from "@/views/Details.vue";
+import { useOrderStore } from "@/stores/modules/orders";
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
     {
-      path: '/',
-      name: 'home',
+      path: "/",
+      name: "home",
       component: Pos,
-      redirect:'/order',
-      children:[
+      redirect: "/order",
+      children: [
         {
-          path:'Order',
-          name:'order',
-          component:Order,
-          async beforeEnter(to,from){
-            const orderStore = useOrderStore()
-            await orderStore.getOrders()
+          path: "Order",
+          name: "order",
+          component: Order,
+          async beforeEnter(to, from) {
+            const orderStore = useOrderStore();
+            await orderStore.getOrders();
           },
-          meta:{
-            transitions:{
-              enter:{
-                name:'',
-                enterClass: 'animate__animated animate__fadeInLeft',
-                leaveClass: ''
-              },
+          meta: {
+            transitions: {
+              enter: {
+                name: "",
+                enterClass: "animate__animated animate__fadeInLeft",
+                leaveClass: ""
+              }
             }
           }
         },
         {
-          path:'Unpaid',
-          name:'unpaid',
-          component:Unpaid,
-          meta:{
-            transitions:{
-              enter:{
-                name:'',
-                enterClass: 'animate__animated animate__fadeInLeft',
-                leaveClass: ''
-              },
+          path: "Unpaid",
+          name: "unpaid",
+          component: Unpaid,
+          meta: {
+            transitions: {
+              enter: {
+                name: "",
+                enterClass: "animate__animated animate__fadeInLeft",
+                leaveClass: ""
+              }
             }
           }
         },
         {
-          path:'Paid',
-          name:'paid',
-          component:Paid,
-          meta:{
-            transitions:{
-              enter:{
-                name:'',
-                enterClass: 'animate__animated animate__fadeInLeft',
-                leaveClass: ''
-              },
+          path: "Paid",
+          name: "paid",
+          component: Paid,
+          meta: {
+            transitions: {
+              enter: {
+                name: "",
+                enterClass: "animate__animated animate__fadeInLeft",
+                leaveClass: ""
+              }
             }
           }
-        },
+        }
       ]
     },
+    {
+      path: "/Details",
+      name: "detailsPdf",
+      component: Details
+    }
   ]
-})
+});
 
-export default router
+export default router;
---
+++ src/views/Details.vue
@@ -0,0 +1,8 @@
+<script setup>
+import { jsPDF } from "jspdf";
+const doc = new jsPDF();
+console.log("doc", doc);
+</script>
+<template>
+  <h1>DetailsPdf</h1>
+</template>
