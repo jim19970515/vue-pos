fd59752636bd2bd8dd788d89e86defac02001656
jim <f0920515972@gmail.com>
2025-06-07 16:13:21 +0800

    feat:安裝swal aleat

--- src/main.ts
+++ src/main.ts
@@ -6,13 +6,16 @@ import { createPinia } from "pinia";
 import App from "./App.vue";
 import router from "./router";
 import VueRouterTransition from "@duannx/vue-router-transition";
+import ElementPlus from "element-plus";
 import "animate.css";
 import jsPDF from "jspdf";
+import Swal from "sweetalert2";
 
 const app = createApp(App);
 const pinia = createPinia();
 
 app.use(pinia);
+app.use(ElementPlus, Swal);
 app.use(jsPDF);
 app.use(router);
 app.use(VueRouterTransition);
