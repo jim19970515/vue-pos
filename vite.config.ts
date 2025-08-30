494d71f962088809bd1c52fef5927eb8f472bdb6
jim <f0920515972@gmail.com>
2025-08-30 13:54:14 +0800

    feat:安裝element plus、jspdf、sweetalert2

--- vite.config.ts
+++ vite.config.ts
@@ -1,16 +1,27 @@
-import { fileURLToPath, URL } from 'node:url'
+import { fileURLToPath, URL } from "node:url";
 
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
+import { defineConfig } from "vite";
+import vue from "@vitejs/plugin-vue";
+import AutoImport from "unplugin-auto-import/vite";
+import Components from "unplugin-vue-components/vite";
+import { ElementPlusResolver } from "unplugin-vue-components/resolvers";
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [
     vue(),
+    AutoImport({
+      resolvers: [ElementPlusResolver()]
+    }),
+    Components({
+      resolvers: [ElementPlusResolver()]
+    })
   ],
   resolve: {
+    // alias 用來定義路徑別名
     alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
+      //import.meta.url 是一個 特殊的變數，它代表目前這個模組檔案的完整 URL。
+      "@": fileURLToPath(new URL("./src", import.meta.url))
     }
   }
-})
+});
