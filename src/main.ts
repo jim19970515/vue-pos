0efede5cd271486a1ba90b3f83ed187e0b8b544e
jim <f0920515972@gmail.com>
2025-11-08 23:41:49 +0800

    fix:測試

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,3 @@
-import "./assets/main.css";
-
 import { createApp } from "vue";
 import { createPinia } from "pinia";
 
@@ -8,6 +6,7 @@ import router from "./router";
 import VueRouterTransition from "@duannx/vue-router-transition";
 import ElementPlus from "element-plus";
 import "animate.css";
+import "./assets/main.css";
 import jsPDF from "jspdf";
 import Swal from "sweetalert2";
 import * as ElementPlusIconsVue from "@element-plus/icons-vue";
