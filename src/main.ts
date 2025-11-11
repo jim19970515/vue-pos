9013282593cade5fbdaae706e3a8476e7d801e78
jim <f0920515972@gmail.com>
2025-11-11 23:34:26 +0800

    feat: 整理main.ts 檔

--- src/main.ts
+++ src/main.ts
@@ -1,28 +1,43 @@
 import { createApp } from "vue";
 import { createPinia } from "pinia";
 
+//vue 生態系
 import App from "./App.vue";
 import router from "./router";
-import VueRouterTransition from "@duannx/vue-router-transition";
-import ElementPlus from "element-plus";
-import "animate.css";
-import "./assets/main.css";
-import jsPDF from "jspdf";
-import Swal from "sweetalert2";
-import * as ElementPlusIconsVue from "@element-plus/icons-vue";
-
-const app = createApp(App);
 const pinia = createPinia();
 
+//=================UI套件=================
+import ElementPlus from "element-plus";
+//element plus icons
+import * as ElementPlusIconsVue from "@element-plus/icons-vue";
+//=================套件=================
+//輪播套件
+import Swiper from "swiper";
+import "swiper/css";
+//彈窗套件
+import Swal from "sweetalert2";
+//pdf套件
+import jsPDF from "jspdf";
+
+import "animate.css";
+import "./assets/main.css";
+import VueRouterTransition from "@duannx/vue-router-transition";
+
+const app = createApp(App);
+
+//=================註冊=================
+// vue 生態系註冊
 app.use(pinia);
-app.use(ElementPlus, Swal);
-app.use(jsPDF);
 app.use(router);
 app.use(VueRouterTransition);
+//ui 套件註冊
+app.use(ElementPlus);
+//套件註冊
+app.use(Swal);
+app.use(jsPDF);
 
 // 註冊 Element Plus Icons
 for (const [key, component] of Object.entries(ElementPlusIconsVue)) {
   app.component(key, component);
 }
-
 app.mount("#app");
