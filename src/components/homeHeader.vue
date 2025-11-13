d39c93bc0f31725d22e75c3dc40a702d6ba6406c
jim <f0920515972@gmail.com>
2025-11-13 23:12:21 +0800

    feat:修改logo圖片大小

--- src/components/homeHeader.vue
+++ src/components/homeHeader.vue
@@ -3,7 +3,7 @@
     <header class="h-fit w-full bg-primary h-fit w-full py-2 px-4">
       <div class="flex justify-between items-center">
         <div class="flex items-center gap-4">
-          <img src="/src/assets/posLogo.png" class="object-cover h-8 sm:h-20" alt="" />
+          <img src="/src/assets/posLogo.png" class="object-cover h-14" alt="" />
           <h1 class="text-3xl text-white font-bold">心心Pos</h1>
         </div>
         <h2 class="text-lg sm:text-2xl text-white font-medium text-center sm:block hidden">
